fdcc885400e270bd4596f5bd187ad294003febc1
YanaGalayko <117485236+YanaGalayko@users.noreply.github.com>
2023-10-26 23:45:16 +0200

    4 tasks completed

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,63 @@
-const key = new Key();
-
-const house = new MyHouse(key);
-const person = new Person(key);
-
-house.openDoor(person.getKey());
-
-house.comeIn(person);
-
-
-export {};
+class Key {
+    private signature: number;
+  
+    constructor() {
+      this.signature = Math.random();
+    }
+  
+    getSignature(): number {
+      return this.signature;
+    }
+  }
+  
+  class Person {
+    private key: Key;
+  
+    constructor(key: Key) {
+      this.key = key;
+    }
+  
+    getKey(): Key {
+      return this.key;
+    }
+  }
+  
+  abstract class House {
+    door: boolean;
+    key: Key;
+    tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.door = false;
+      this.key = key;
+    }
+  
+    comeIn(person: Person): void {
+      this.door
+        ? this.tenants.push(person)
+        : alert("Sorry, but the door is closed");
+    }
+  
+    abstract openDoor(key: Key): void;
+  }
+  
+  class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+      } else {
+        this.door = false;
+      }
+    }
+  }
+  
+  const key = new Key();
+  
+  const house = new MyHouse(key);
+  const person = new Person(key);
+  
+  house.openDoor(person.getKey());
+  
+  house.comeIn(person);
+  
+  export {};
